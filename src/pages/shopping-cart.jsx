f4f6036cac6df7821ce6134c83e2559949fca9db
LuisArmando-TestCoder <luisarmando.murillobaltodano@gmail.com>
2019-11-28 19:04:17 -0600

    add: success and fail

--- src/pages/shopping-cart.jsx
+++ src/pages/shopping-cart.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import StripeCheckout from "react-stripe-checkout";
+import { NavLink } from 'react-router-dom';
+import StripeCheckout from 'react-stripe-checkout';
 import Layout from '../components/layout';
 import CartProduct from '../components/cart-product';
 import movePropValue from '../services/movePropValue';
@@ -8,6 +9,26 @@ import getItemsSummation from '../services/getItemsSummation';
 import checkout from '../services/checkout';
 import './checkout.css';
 
+const GoBackInStatus = (props) => {
+    const states = {
+        success: <React.Fragment>
+            <p>The 😏 checkout 😊 was successful!😀</p>
+            <NavLink to='/'>Let's buy even MORE!</NavLink>
+        </React.Fragment>,
+        error: <React.Fragment>
+            <p>It seems that the operation failed</p>
+            <NavLink to='/'>Try again</NavLink>
+        </React.Fragment>,
+        wait: <p>Loading</p>
+    };
+    return (
+        <div>
+            <h2>{props.status}</h2>
+            {states[props.status]}
+        </div>
+    )
+}
+
 export default props => {
     const [products, setProducts] = useState(props.cart);
     const [checkoutProduct, setCheckoutProduct] = useState({
@@ -15,32 +36,7 @@ export default props => {
         price: getSubtotal(),
         description: 'ecommerce clothes'
     });
-    const [modal, setModal] = useState({
-        show: false,
-        message: 'Buy every item in cart',
-        children: (<StripeCheckout stripeKey="pk_test_rqlF5WYlPXEKcONrl1MqUFte00ZZB9qxgQ"
-                                    token={(token, addresses) => {
-                                        checkout(token, addresses, checkoutProduct);
-                                        props.cart.splice(0, Infinity);
-                                        setProducts([...props.cart]);
-                                        modal.show = false;
-                                        setModal({...modal});
-                                    }}
-                                    amount={checkoutProduct.price * 100}
-                                    name="Ecommerce products"
-                                    billingAddress
-                                    shippingAddress
-                    />),
-        buttons: [
-            {
-                action() {
-                    modal.show = false;
-                    setModal({...modal});
-                },
-                content: 'Finish',
-            }
-        ]
-    });
+    const [displayStatus, setDisplayStatus] = useState();
 
     function getSubtotal() {
         return products.length ? products.map(a => a.itemsAmount * a.price).reduce((a, b) => a + b) : 0;
@@ -54,22 +50,30 @@ export default props => {
     }
 
     return (
-        <Layout modal={modal} itemsQuantity={getItemsSummation(props.cart, 'itemsAmount')}>
+        <Layout itemsQuantity={getItemsSummation(props.cart, 'itemsAmount')}>
             {products.length ?
                 <div className='checkout--sticky'>
                     <div className='checkout--sticky__wrapper'>
                         <i className='wrapper__info'>Subtotal: {' '}
                             <b className='info__value'>${getSubtotal()}</b>
                         </i>
-                        <button className='wrapper__btn' onClick={() => {
-                            modal.show = true;
-                            setModal({...modal});
-                        }}>Checkout</button>
+                        <StripeCheckout stripeKey='pk_test_rqlF5WYlPXEKcONrl1MqUFte00ZZB9qxgQ'
+                                    token={(token, addresses) => {
+                                        setDisplayStatus(<GoBackInStatus status={'wait'}/>);
+                                        checkout(token, addresses, checkoutProduct).then(r => setDisplayStatus(<GoBackInStatus {...r}/>));
+                                        props.cart.splice(0, Infinity);
+                                        setProducts([...props.cart]);
+                                    }}
+                                    amount={checkoutProduct.price * 100}
+                                    name='Ecommerce products'
+                                    billingAddress
+                                    shippingAddress
+                        />
                     </div>
                 </div>
             : null}
 
-            <h2>Cart</h2>
+            <h2>{displayStatus || 'Cart'}</h2>
             <div className='products'>
                 {products.map((product, i) => <CartProduct key={i}
                     trigger={saveChanges}
--- src/services/checkout.js
+++ src/services/checkout.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-export default (token, addresses, product) => {
+export default (token, addresses, product) => new Promise((resolve, reject) => {
     axios.post('https://l5d4l.sse.codesandbox.io/checkout',
         { token, product }
-    );
-}
+    ).then(r => resolve(r.data));
+});
